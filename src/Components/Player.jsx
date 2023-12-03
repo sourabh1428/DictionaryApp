ea0ac8457ea415fb45bfbaf04f30722a1082fbd9
sourabh1428 <sppathak1428@gmail.com>
2023-12-03 18:01:07 +0530

    dsa

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,14 +1,12 @@
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-// import 'react-h5-audio-player/lib/styles.less' Use LESS
-// import 'react-h5-audio-player/src/styles.scss' Use SASS
 
  const Player = ({url}) => (
   <AudioPlayer
     
     src={url}
     onPlay={e => console.log("onPlay")}
-    // other props here
+    
   />
 );
 export default Player;
